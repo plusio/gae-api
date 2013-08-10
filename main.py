1176905c5c1aec7557d245eeff0a14615678f840
AJ <ajrahim@AJs-MacBook-Pro.local>
2013-08-10 17:21:57 -0400

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+SECRET_KEY = "inb035hb05hbnh52lh4ng49"
+
 #!/usr/bin/env python
 #
 #
@@ -5,7 +7,7 @@
 #
 #
 #   Author : AJ Rahim @ Citizenhub
-#   Support : open.plus.io/+docs
+#   Support : www.plus.io/+docs
 #
 #
 # Copyright 2013 Citizenhub, Inc.
@@ -55,12 +57,14 @@ from google.appengine.ext.db import Key
 from google.appengine.ext.db.metadata import Kind
 from google.appengine.api.runtime import runtime
 from google.appengine.api import oauth
+from google.appengine.api import urlfetch
 
 import os
 import cloudstorage
 import webapp2
 import json
 import urllib2
+import jinja2
 
 
 
@@ -80,9 +84,14 @@ class CustomCollection(db.Expando):
 ###     When you view the webpage from the internet      ###
 
 
+jinja_environment = jinja2.Environment(autoescape=True,
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__))))
+
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        pass
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render())
 
 
 ###     Get information on the app engine app itself (To Be Expanded)     ###
@@ -90,30 +99,39 @@ class MainHandler(webapp2.RequestHandler):
 
 class AppInfo(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write("%s({" % urllib2.unquote(self.request.get('callback')))
-        self.response.out.write("'cpu_usage':'%s'," % runtime.cpu_usage())
-        self.response.out.write("})")
+        if(self.request.get('secret_key') != SECRET_KEY):
+            pass
+        else:
+            self.response.out.write("%s({" % urllib2.unquote(self.request.get('callback')))
+            self.response.out.write("'cpu_usage':'%s'," % runtime.cpu_usage())
+            self.response.out.write("})")
 
 
 ###     Getting data from Google Cloud Storage / Allow user to delete files from Cloud Storage     ###
 
 class Bucket(webapp2.RequestHandler):
     def get(self, bucket):
-        self.response.out.write("%s([" % urllib2.unquote(self.request.get('callback')))
-        stats = cloudstorage.listbucket('/%s' % bucket)
-        count = 0
-        for stat in stats:
-            count += 1
-            self.response.write("{'file' : \"%s\"}," % stat.filename)
-        self.response.out.write("])")
-    def delete(self, bucket):
-        if self.request.get("file") != "":
-            try:
-                cloudstorage.delete(self.request.get("file"))
-            except cloudstorage.NotFoundError:
-                pass
-        else:
+        if(self.request.get('secret_key') != SECRET_KEY):
             pass
+        else:
+            self.response.out.write("%s([" % urllib2.unquote(self.request.get('callback')))
+            stats = cloudstorage.listbucket('/%s' % bucket)
+            count = 0
+            for stat in stats:
+                count += 1
+                self.response.write("{'file' : \"%s\"}," % stat.filename)
+            self.response.out.write("])")
+    def delete(self, bucket):
+        if(self.request.get('secret_key') != SECRET_KEY):
+            pass
+        else:
+            if self.request.get("file") != "":
+                try:
+                    cloudstorage.delete(self.request.get("file"))
+                except cloudstorage.NotFoundError:
+                    pass
+            else:
+                pass
 
 
 ###     Retrive and insert data in the main Structure      ###
@@ -123,22 +141,25 @@ class Structure(webapp2.RequestHandler):
     # Get rows from the structure kind by specifying a key_name
 
     def get(self):
-        m = metadata.get_kinds()
-        arr = {}
-        count = 0
-        for n in m:
-            if n.startswith("_"):
-                pass
-            else:
-                arr[count] = n
-                count += 1
+        if(self.request.get('secret_key') != SECRET_KEY):
+            pass
+        else:
+            m = metadata.get_kinds()
+            arr = {}
+            count = 0
+            for n in m:
+                if n.startswith("_"):
+                    pass
+                else:
+                    arr[count] = n
+                    count += 1
 
-        # RESPONSE === BEGIN #
-        self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write("%s(" % urllib2.unquote(self.request.get('callback')))
-        self.response.out.write(json.dumps(arr))
-        self.response.out.write(")")
-        # RESPONSE === END #
+            # RESPONSE === BEGIN #
+            self.response.headers['Content-Type'] = 'application/json'
+            self.response.out.write("%s(" % urllib2.unquote(self.request.get('callback')))
+            self.response.out.write(json.dumps(arr))
+            self.response.out.write(")")
+            # RESPONSE === END #
 
 
 class StructureKey(webapp2.RequestHandler):
@@ -147,46 +168,49 @@ class StructureKey(webapp2.RequestHandler):
 
     def get(self, collection):
 
-        global currentcollection
-        currentcollection = collection.lower()
-        c = CustomCollection()
-
-        c.kind()
-        n = c.all()
-
-        if self.request.get("filter") != "":
-            n.filter("%s" % self.request.get("filter"), "%s" %  self.request.get("value").replace(' ', '_'))
-        else:
+        if(self.request.get('secret_key') != SECRET_KEY):
             pass
-
-        if self.request.get("offset") != "":
-            fil = n.fetch(int(self.request.get("limit")), int(self.request.get("offset")))
-        elif self.request.get("limit") != "":
-            fil = n.fetch(int(self.request.get("limit")))
         else:
-            fil = n
+            global currentcollection
+            currentcollection = collection.lower()
+            c = CustomCollection()
 
-        r = fil.count()
+            c.kind()
+            n = c.all()
 
-        m = metadata.get_properties_of_kind(collection.lower())
-        arr = {}
-        full = []
-        count = 0
-        for ab in m:
-            arr[count] = ab
-            count += 1
-        arr[count] = "id"
-        full.append(arr)
-        string = {}
-        string["count"] = r
-        full.append(string)
+            if self.request.get("filter") != "":
+                n.filter("%s" % self.request.get("filter"), "%s" %  self.request.get("value").replace(' ', '_'))
+            else:
+                pass
 
-        # RESPONSE === BEGIN #
-        self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write("%s(" % urllib2.unquote(self.request.get('callback')))
-        self.response.out.write(json.dumps(full))
-        self.response.out.write(")")
-        # RESPONSE === END #
+            if self.request.get("offset") != "":
+                fil = n.fetch(int(self.request.get("limit")), int(self.request.get("offset")))
+            elif self.request.get("limit") != "":
+                fil = n.fetch(int(self.request.get("limit")))
+            else:
+                fil = n
+
+            r = fil.count()
+
+            m = metadata.get_properties_of_kind(collection.lower())
+            arr = {}
+            full = []
+            count = 0
+            for ab in m:
+                arr[count] = ab
+                count += 1
+            arr[count] = "id"
+            full.append(arr)
+            string = {}
+            string["count"] = r
+            full.append(string)
+
+            # RESPONSE === BEGIN #
+            self.response.headers['Content-Type'] = 'application/json'
+            self.response.out.write("%s(" % urllib2.unquote(self.request.get('callback')))
+            self.response.out.write(json.dumps(full))
+            self.response.out.write(")")
+            # RESPONSE === END #
         
 
 ###     Retrive and insert data in a specified Collection      ###
@@ -196,119 +220,134 @@ class Collection(webapp2.RequestHandler):
     # Get a list from a specified collection
 
     def get(self, collection):
-        self.response.headers['Access-Control-Allow-Origin'] = '*'
-        global currentcollection
-        currentcollection = collection.lower()
-        c = CustomCollection()
-        c.kind()
-        n = c.all()
-
-        if self.request.get("filter") != "":
-            n.filter("%s" % self.request.get("filter"), "%s" %  self.request.get("value").replace(' ', '_'))
-        else:
+        if(self.request.get('secret_key') != SECRET_KEY):
             pass
-
-        if self.request.get("offset") != "":
-            fil = n.fetch(int(self.request.get("limit")), int(self.request.get("offset")))
-        elif self.request.get("limit") != "":
-            fil = n.fetch(int(self.request.get("limit")))
         else:
-            fil = n
+            self.response.headers['Access-Control-Allow-Origin'] = '*'
+            global currentcollection
+            currentcollection = collection.lower()
+            c = CustomCollection()
+            c.kind()
+            n = c.all()
 
-        b = metadata.get_properties_of_kind(collection.lower())
-        arr = []
+            if self.request.get("filter") != "":
+                n.filter("%s" % self.request.get("filter"), "%s" %  self.request.get("value").replace(' ', '_'))
+            else:
+                pass
 
-        for g in fil:
-            results = {}
-            results["id"] = g.key().id()
-            for a in b:
-                try:
-                    results[a] = getattr(g,a)
-                except:
-                    pass
+            if self.request.get("offset") != "":
+                fil = n.fetch(int(self.request.get("limit")), int(self.request.get("offset")))
+            elif self.request.get("limit") != "":
+                fil = n.fetch(int(self.request.get("limit")))
+            else:
+                fil = n
 
-            arr.append(results)
+            b = metadata.get_properties_of_kind(collection.lower())
+            arr = []
 
-        # RESPONSE === BEGIN #
-        self.response.out.write("%s(" % urllib2.unquote(self.request.get('callback')))
-        self.response.out.write(json.dumps(arr))
-        self.response.out.write(")")
-        # RESPONSE === END #
+            for g in fil:
+                results = {}
+                results["id"] = g.key().id()
+                for a in b:
+                    try:
+                        results[a] = getattr(g,a)
+                    except:
+                        pass
+
+                arr.append(results)
+
+            # RESPONSE === BEGIN #
+            self.response.out.write("%s(" % urllib2.unquote(self.request.get('callback')))
+            self.response.out.write(json.dumps(arr))
+            self.response.out.write(")")
+            # RESPONSE === END #
     
     # Insert data into a specified collection
 
     def post(self, collection):
         self.response.headers['Access-Control-Allow-Origin'] = '*'
         content = json.loads(self.request.body)
-        global currentcollection
-        currentcollection = collection.lower()
-        c = CustomCollection()
-        c.kind()  
+        if(self.request.get('secret_key') != SECRET_KEY):
+            pass
+        else:
+            global currentcollection
+            currentcollection = collection.lower()
+            c = CustomCollection()
+            c.kind()  
 
-        for (x,y) in content.iteritems():
-            setattr(c,x,y)
+            for (x,y) in content.iteritems():
+                setattr(c,x,y)
 
-        c.put()
+            c.put()
 
-        # RESPONSE === BEGIN #
-        self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write('collection %s : PUT' % collection)
-        # RESPONSE === END #
+            # RESPONSE === BEGIN #
+            self.response.headers['Content-Type'] = 'application/json'
+            self.response.out.write('collection %s : PUT' % collection)
+            # RESPONSE === END #
 
 ###     Get data from a collection based on an ID value      ###
 
 class CollectionItem(webapp2.RequestHandler):
 
     def get(self, collection, item):
-        global currentcollection
-        currentcollection = collection.lower()
-        c = CustomCollection()
-        c.kind()
-        n = c.all().filter("__key__ =", db.Key.from_path(collection, int(item)))
-        b = metadata.get_properties_of_kind(collection.lower())
-        arr = {}
+        if(self.request.get('secret_key') != SECRET_KEY):
+            pass
+        else:
+            global currentcollection
+            currentcollection = collection.lower()
+            c = CustomCollection()
+            c.kind()
+            n = c.all().filter("__key__ =", db.Key.from_path(collection, int(item)))
+            b = metadata.get_properties_of_kind(collection.lower())
+            arr = {}
 
-        for g in n:
-            for a in b:
-                try:
-                    arr[a.lower()] = getattr(g,a)
-                except:
-                    pass
+            for g in n:
+                for a in b:
+                    try:
+                        arr[a.lower()] = getattr(g,a)
+                    except:
+                        pass
 
-        # RESPONSE === BEGIN #
-        self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write("%s(" % urllib2.unquote(self.request.get('callback')))
-        self.response.out.write(json.dumps(arr))
-        self.response.out.write(")")
-        # RESPONSE === END #
+            # RESPONSE === BEGIN #
+            self.response.headers['Content-Type'] = 'application/json'
+            self.response.out.write("%s(" % urllib2.unquote(self.request.get('callback')))
+            self.response.out.write(json.dumps(arr))
+            self.response.out.write(")")
+            # RESPONSE === END #
 
     def post(self, collection, item):
         content = json.loads(self.request.body)
-        global currentcollection
-        currentcollection = collection.lower()
-        c = CustomCollection()
-        c.kind()
-        n = c.all().filter("__key__ =", db.Key.from_path(collection, int(item)))
+        if(self.request.get('secret_key') != SECRET_KEY):
+            pass
+        else:
+            global currentcollection
+            currentcollection = collection.lower()
+            c = CustomCollection()
+            c.kind()
+            n = c.all().filter("__key__ =", db.Key.from_path(collection, int(item)))
 
-        for g in n:
-            for (x,y) in content.iteritems():
-                setattr(g,x.lower(),y)
-            g.put()
+            for g in n:
+                for (x,y) in content.iteritems():
+                    setattr(g,x.lower(),y)
+                g.put()
 
     def delete(self, collection, item):
-        global currentcollection
-        currentcollection = collection.lower()
-        c = CustomCollection()
-        c.kind()
-        n = c.all().filter("__key__ =", db.Key.from_path(collection, int(item)))
-        db.delete(n)
+        if(self.request.get('secret_key') != SECRET_KEY):
+            pass
+        else:
+            global currentcollection
+            currentcollection = collection.lower()
+            c = CustomCollection()
+            c.kind()
+            n = c.all().filter("__key__ =", db.Key.from_path(collection, int(item)))
+            db.delete(n)
 
-        # RESPONSE === BEGIN #
-        self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write("%s(" % urllib2.unquote(self.request.get('callback')))
-        self.response.out.write("{'delete':'successful'}")
-        self.response.out.write(")")
-        # RESPONSE === END #
+            # RESPONSE === BEGIN #
+            self.response.headers['Content-Type'] = 'application/json'
+            self.response.out.write("%s(" % urllib2.unquote(self.request.get('callback')))
+            self.response.out.write("{'delete':'successful'}")
+            self.response.out.write(")")
+            # RESPONSE === END #
 
 
 
